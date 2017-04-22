9a81a28749e2a0d6c5e77bc4ad96e7a9076c56fc
maskwang520 <826655812@qq.com>
2017-04-22 16:53:04 +0800

    four

---
+++ Search for a Range.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h> 
+#include<stdlib.h>
+int binarySearch(int* nums,int target,int len){
+	int height=len-1;
+	int low=0,medium;
+	while(low<=height){
+		medium=(low+height)/2;
+		if(nums[medium]==target){
+			return medium;
+		}
+		else
+		  if(nums[medium]>target)
+		     height=medium-1;
+		  else
+		      low=medium+1;
+	}
+	return -1;
+}
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int *b,result,start,end,k,flag=0,flag1=0;
+    b=(int *)malloc(sizeof(int)*2);
+    result=binarySearch(nums,target,numsSize);
+    start=result;
+    end=result;
+    if(result==-1){
+    	*returnSize=2;
+    	b[0]=-1;
+    	b[1]=-1;
+    	return b;
+	}
+	else{
+		for(k=1;k<numsSize;k++){
+			if(result-k>=0&&nums[result-k]==target){
+				start--;
+				flag=1;
+			}
+			  
+			 if(result+k<numsSize&&nums[result+k]==target){
+			 	end++; 
+			 	flag1=1;
+			 }
+			if(flag==0&&flag1==0)
+			  break;
+		} 
+	}
+	*returnSize=2;
+    b[0]=start;
+    b[1]=end;
+    return b;
+}
+int main(){
+	int a[6]={5,7,7,8,8,10},b[2],*bpoint,*returnSizePoint,returnSize;
+	bpoint=b;
+	returnSizePoint=&returnSize;
+	bpoint=searchRange(a, 6, 8, returnSizePoint); 
+	for(int i=0;i<2;i++){
+		printf("%d\t",bpoint[i]); 
+	}
+}
